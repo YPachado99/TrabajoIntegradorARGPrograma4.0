2325a3deb89e43db34814f078ad48a850214f884
demonjl <72841238+demonjl@users.noreply.github.com>
2023-04-26 08:53:16 -0300

    Cambios en general de la pagina tanto en estilo y manipulacion de peticion con AJAX (#9)
    * nuevos cambios pequeños en las secciones hero y api
    
    * cambios en todas las sections del index.html, cambios en style.css y agrego peticion GET con AJAX como comentario

--- js/script.js
+++ js/script.js
@@ -43,6 +43,15 @@ const users_github = () => {
 }
 users_github()
 
+// const users_list_ajax = $.ajax({
+//                             method: "GET",
+//                             url: "https://reqres.in/api/users?page=1",
+//                         })
+//                         .done(function( result ) {
+//                             let datos=result.data;   
+//                             users_data(datos)
+//                         });
+
 const users_list =  fetch('https://reqres.in/api/users?page=1')
                     .then(response=>response.json())
                     .then(data=>users_data(data.data))
@@ -71,8 +80,6 @@ const users_data = (data) => {
     });   
 }
 
-
-
 $(document).on("scroll", () => {
     if($(document).scrollTop() > 300){
         $('.navbar-lado__derecho').css('background-color','white');
@@ -81,180 +88,191 @@ $(document).on("scroll", () => {
         $('.navbar-lado__derecho').css('background-color','var(--color-amarillo)');
     }
 });
+
 $("#textomostrar").hide();
-  $("#Mostrar").hide();
-  $("#Ocultar").on("click", function () {
-    $(".wizard-section").hide();
-    $("#textomostrar").show();
-  });
-  $("#Mostrar").on("click", function () {
+$('#form_process__textomostrar').hide();
+$("#Mostrar").hide();
+
+$("#Ocultar").on("click", function () {
+    $("#formulario-form_process").hide();
+    $("#form_process__textomostrar").show();
+});
+
+$("#Mostrar").on("click", function () {
     $("#formulario").show();
     $("#textomostrar").hide();
     $("#Mostrar").hide();
-  });
-  $("#Enviar").on("click", function (event) {
+});
+$("#Enviar").on("click", function (event) {
     if ($("#formulario").valid() == true ) {
-      $("#textomostrar").show();
-      $( "#Mostrar").show();
-      $("#formulario").hide();
+        $("#textomostrar").show();
+        $( "#Mostrar").show();
+        $("#formulario").hide();
     } 
-  });
-  var rules_name = {
-    required: true,
-      minlength: 3,
-  };
-    $("#formulario").validate({
-      rules: {
-        nombre : rules_name,
-        apellido: rules_name,
-        mail: {
-          required: true,
-          email: true,
-        },
-        telefono:{
-          required: true,
-        }
-      },
-      messages : {
-        nombre: {
-          required: "Este campo es requerido",
-          minlength: "Minimo 3 caracteres",
-        },
-        apellido: {
-          required: "Este campo es requerido",
-          minlength: "Minimo 3 caracteres"
-        },
-        mail: {
-          required: "Este campo es requerido",
-          email: "El formato de email debe ser: abc@gmail.com"
-        },
-        telefono: {
-          required: "Este campo es requerido",
-        }
+});
 
-      },
-      errorPlacement: function(error, element) {
-        if (element.attr("name") == "nombre")
-            {
-              error.appendTo($('#errorName'));
-            } else if (element.attr("name") == "apellido") {
-              error.appendTo($('#errorSurname'))
-            } else {
-              error.insertAfter(element);
-            }
-      }
-    });
-    $("#formulario").keyup(function () {
-      if ($("#Nombre").valid() == true ) {
-        $( "#Nombre" ).addClass( "is-valid" );
-      } else {
-        $( "#Nombre" ).removeClass( "is-valid" );
-      }
-      if ($("#Apellido").valid() == true ) {
-        $( "#Apellido" ).addClass( "is-valid" );
-      } else {
-        $( "#Apellido" ).removeClass( "is-valid" );
-      }
-      if ($("#Email").valid() == true ) {
-        $( "#Email" ).addClass( "is-valid" );
-      } else {
-        $( "#Email" ).removeClass( "is-valid" );
-      }
-      if ($("#Telefono").valid() == true ) {
-        $( "#Telefono" ).addClass( "is-valid" );
-      } else {
-        $( "#Telefono" ).removeClass( "is-valid" );
-      }
-    });
-    jQuery('.form-wizard-next-btn').click(function() {
-      var parentFieldset = jQuery(this).parents('.wizard-fieldset');
-      var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
-      var next = jQuery(this);
-      var nextWizardStep = true;
-      parentFieldset.find('.wizard-required').each(function(){
-        var thisValue = jQuery(this).val();
-  
-        if( thisValue == "") {
-          jQuery(this).siblings(".wizard-form-error").slideDown();
-          nextWizardStep = false;
+var rules_name = {
+    required: true,
+    minlength: 3,
+};
+
+$("#formulario").validate({
+    rules: {
+    nombre : rules_name,
+    apellido: rules_name,
+    mail: {
+        required: true,
+        email: true,
+    },
+    telefono:{
+        required: true,
+    }
+    },
+    messages : {
+    nombre: {
+        required: "Este campo es requerido",
+        minlength: "Minimo 3 caracteres",
+    },
+    apellido: {
+        required: "Este campo es requerido",
+        minlength: "Minimo 3 caracteres"
+    },
+    mail: {
+        required: "Este campo es requerido",
+        email: "El formato de email debe ser: abc@gmail.com"
+    },
+    telefono: {
+        required: "Este campo es requerido",
+    }
+
+    },
+    errorPlacement: function(error, element) {
+    if (element.attr("name") == "nombre")
+        {
+            error.appendTo($('#errorName'));
+        } else if (element.attr("name") == "apellido") {
+            error.appendTo($('#errorSurname'))
+        } else {
+            error.insertAfter(element);
         }
-        else {
-          jQuery(this).siblings(".wizard-form-error").slideUp();
-        }
-      });
-      if( nextWizardStep) {
-        next.parents('.wizard-fieldset').removeClass("show","400");
-        currentActiveStep.removeClass('active').addClass('activated').next().addClass('active',"400");
-        next.parents('.wizard-fieldset').next('.wizard-fieldset').addClass("show","400");
-        jQuery(document).find('.wizard-fieldset').each(function(){
-          if(jQuery(this).hasClass('show')){
-            var formAtrr = jQuery(this).attr('data-tab-content');
-            jQuery(document).find('.form-wizard-steps .form-wizard-step-item').each(function(){
-              if(jQuery(this).attr('data-attr') == formAtrr){
-                jQuery(this).addClass('active');
-                var innerWidth = jQuery(this).innerWidth();
-                var position = jQuery(this).position();
-                jQuery(document).find('.form-wizard-step-move').css({"left": position.left, "width": innerWidth});
-              }else{
-                jQuery(this).removeClass('active');
-              }
-            });
-          }
-        });
-      }
+    }
+});
+
+$("#formulario").keyup(function () {
+    if ($("#Nombre").valid() == true ) {
+    $( "#Nombre" ).addClass( "is-valid" );
+    } else {
+    $( "#Nombre" ).removeClass( "is-valid" );
+    }
+    if ($("#Apellido").valid() == true ) {
+    $( "#Apellido" ).addClass( "is-valid" );
+    } else {
+    $( "#Apellido" ).removeClass( "is-valid" );
+    }
+    if ($("#Email").valid() == true ) {
+    $( "#Email" ).addClass( "is-valid" );
+    } else {
+    $( "#Email" ).removeClass( "is-valid" );
+    }
+    if ($("#Telefono").valid() == true ) {
+    $( "#Telefono" ).addClass( "is-valid" );
+    } else {
+    $( "#Telefono" ).removeClass( "is-valid" );
+    }
+});
+
+jQuery('.form-wizard-next-btn').click(function() {
+    var parentFieldset = jQuery(this).parents('.wizard-fieldset');
+    var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
+    var next = jQuery(this);
+    var nextWizardStep = true;
+    parentFieldset.find('.wizard-required').each(function(){
+    var thisValue = jQuery(this).val();
+
+    if( thisValue == "") {
+        jQuery(this).siblings(".wizard-form-error").slideDown();
+        nextWizardStep = false;
+    }
+    else {
+        jQuery(this).siblings(".wizard-form-error").slideUp();
+    }
     });
-    jQuery('.form-wizard-previous-btn').click(function() {
-      var counter = parseInt(jQuery(".wizard-counter").text());;
-      var prev =jQuery(this);
-      var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
-      prev.parents('.wizard-fieldset').removeClass("show","400");
-      prev.parents('.wizard-fieldset').prev('.wizard-fieldset').addClass("show","400");
-      currentActiveStep.removeClass('active').prev().removeClass('activated').addClass('active',"400");
-      jQuery(document).find('.wizard-fieldset').each(function(){
+    if( nextWizardStep) {
+    next.parents('.wizard-fieldset').removeClass("show","400");
+    currentActiveStep.removeClass('active').addClass('activated').next().addClass('active',"400");
+    next.parents('.wizard-fieldset').next('.wizard-fieldset').addClass("show","400");
+    jQuery(document).find('.wizard-fieldset').each(function(){
         if(jQuery(this).hasClass('show')){
-          var formAtrr = jQuery(this).attr('data-tab-content');
-          jQuery(document).find('.form-wizard-steps .form-wizard-step-item').each(function(){
+        var formAtrr = jQuery(this).attr('data-tab-content');
+        jQuery(document).find('.form-wizard-steps .form-wizard-step-item').each(function(){
             if(jQuery(this).attr('data-attr') == formAtrr){
-              jQuery(this).addClass('active');
-              var innerWidth = jQuery(this).innerWidth();
-              var position = jQuery(this).position();
-              jQuery(document).find('.form-wizard-step-move').css({"left": position.left, "width": innerWidth});
+            jQuery(this).addClass('active');
+            var innerWidth = jQuery(this).innerWidth();
+            var position = jQuery(this).position();
+            jQuery(document).find('.form-wizard-step-move').css({"left": position.left, "width": innerWidth});
             }else{
-              jQuery(this).removeClass('active');
+            jQuery(this).removeClass('active');
             }
-          });
+        });
         }
-      });
     });
-    jQuery(document).on("click",".form-wizard .form-wizard-submit" , function(){
-      var parentFieldset = jQuery(this).parents('.wizard-fieldset');
-      var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
-      parentFieldset.find('.wizard-required').each(function() {
-        var thisValue = jQuery(this).val();
-        if( thisValue == "" ) {
-          jQuery(this).siblings(".wizard-form-error").slideDown();
+    }
+});
+
+jQuery('.form-wizard-previous-btn').click(function() {
+    var counter = parseInt(jQuery(".wizard-counter").text());;
+    var prev =jQuery(this);
+    var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
+    prev.parents('.wizard-fieldset').removeClass("show","400");
+    prev.parents('.wizard-fieldset').prev('.wizard-fieldset').addClass("show","400");
+    currentActiveStep.removeClass('active').prev().removeClass('activated').addClass('active',"400");
+    jQuery(document).find('.wizard-fieldset').each(function(){
+    if(jQuery(this).hasClass('show')){
+        var formAtrr = jQuery(this).attr('data-tab-content');
+        jQuery(document).find('.form-wizard-steps .form-wizard-step-item').each(function(){
+        if(jQuery(this).attr('data-attr') == formAtrr){
+            jQuery(this).addClass('active');
+            var innerWidth = jQuery(this).innerWidth();
+            var position = jQuery(this).position();
+            jQuery(document).find('.form-wizard-step-move').css({"left": position.left, "width": innerWidth});
+        }else{
+            jQuery(this).removeClass('active');
         }
-        else {
-          jQuery(this).siblings(".wizard-form-error").slideUp();
-        }
-      });
+        });
+    }
     });
-    jQuery(".form-control").on('focus', function(){
-      var tmpThis = jQuery(this).val();
-      if(tmpThis == '' ) {
-        jQuery(this).parent().addClass("focus-input");
-      }
-      else if(tmpThis !='' ){
-        jQuery(this).parent().addClass("focus-input");
-      }
-    }).on('blur', function(){
-      var tmpThis = jQuery(this).val();
-      if(tmpThis == '' ) {
-        jQuery(this).parent().removeClass("focus-input");
-        jQuery(this).siblings('.wizard-form-error').slideDown("3000");
-      }
-      else if(tmpThis !='' ){
-        jQuery(this).parent().addClass("focus-input");
-        jQuery(this).siblings('.wizard-form-error').slideUp("3000");
-      }
+});
+
+jQuery(document).on("click",".form-wizard .form-wizard-submit" , function(){
+    var parentFieldset = jQuery(this).parents('.wizard-fieldset');
+    var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
+    parentFieldset.find('.wizard-required').each(function() {
+    var thisValue = jQuery(this).val();
+    if( thisValue == "" ) {
+        jQuery(this).siblings(".wizard-form-error").slideDown();
+    }
+    else {
+        jQuery(this).siblings(".wizard-form-error").slideUp();
+    }
     });
+});
+
+jQuery(".form-control").on('focus', function(){
+    var tmpThis = jQuery(this).val();
+    if(tmpThis == '' ) {
+    jQuery(this).parent().addClass("focus-input");
+    }
+    else if(tmpThis !='' ){
+    jQuery(this).parent().addClass("focus-input");
+    }
+}).on('blur', function(){
+    var tmpThis = jQuery(this).val();
+    if(tmpThis == '' ) {
+    jQuery(this).parent().removeClass("focus-input");
+    jQuery(this).siblings('.wizard-form-error').slideDown("3000");
+    }
+    else if(tmpThis !='' ){
+    jQuery(this).parent().addClass("focus-input");
+    jQuery(this).siblings('.wizard-form-error').slideUp("3000");
+    }
+});
